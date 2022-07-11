aa67116d8a2ee38de944a56f8ea822cd9da0144d
YSBINN <dowkdg@naver.com>
2022-07-11 19:00:36 +0900

    Add : pull_request_template.md 파일추가

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import css from './Homepage.module.scss';
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className={css.container}>
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1557827983-012eb6ea8dc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bHVzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+      <div className={css.pinModal}></div>
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1600277259675-0edbcb3182d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bHVzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1532796107-d570a19405ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bHVzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://i.pinimg.com/474x/5d/80/dc/5d80dc96b01e9caddd299338a4fa2d75.jpg"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1533749968753-1a9994823766?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGx1c2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b2NlYW4lMjBiZWFjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8b2NlYW4lMjBiZWFjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1420593248178-d88870618ca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGx1c2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+      />
+
+      <img
+        alt="핀이미지"
+        src="https://images.unsplash.com/photo-1625887803552-9a80d55f37fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGx1c2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+      />
+    </div>
+  );
 }
 
 export default Homepage;
