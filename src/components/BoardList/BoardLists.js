f9951cb000cdf42801d30ad98edc1bf5479eeb3a
YSBINN <dowkdg@naver.com>
2022-07-30 21:28:08 +0900

    fix : 조건부 랜더링을 통한 보드리스트 핸들링 추가

--- src/components/BoardList/BoardLists.js
+++ src/components/BoardList/BoardLists.js
@@ -3,18 +3,22 @@ import BoardList from './BoardList';
 import css from './BoardLists.module.scss';
 
 function BoardLists({ data, title, setBoardId }) {
-  return (
-    <div className={css.boardListModal}>
-      {data.map(boardsdata => (
-        <BoardList
-          key={boardsdata.board_id}
-          boardsdata={boardsdata}
-          title={title}
-          setBoardId={setBoardId}
-        />
-      ))}
-    </div>
-  );
+  if (data.length === 0) {
+    return <></>;
+  } else {
+    return (
+      <div className={css.boardListModal}>
+        {data.map(boardsdata => (
+          <BoardList
+            key={boardsdata.board_id}
+            boardsdata={boardsdata}
+            title={title}
+            setBoardId={setBoardId}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default BoardLists;
